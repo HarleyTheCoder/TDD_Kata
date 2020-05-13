1c73c7506424378c1477e085954e5108f3b45f1a
harley <harleymakesart@gmail.com>
2020-05-13 19:31:22 -0400

    Initial commit.

---
+++ src/StringCalc.java
@@ -0,0 +1,118 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+//I was not in class to learn about this and can't view the lecture, so I did my best.
+
+//I could not get the assertions to run automatically so I did what I could
+
+public class StringCalc {
+
+	public static void main(String[] args) {
+
+		
+	}
+	
+	//Idea: replace all non digits with ~, then split into array
+	public static int add(String s) {
+		
+		if (s.contentEquals("")) { //If it's empty, return 0
+			return 0;
+		}
+		
+		
+		//try all this for negatives, use same as the later code, to ensure this goes right
+		List<Integer> negatives = new ArrayList<>();
+		try {
+			String sNegatives = s.replaceAll("[^\\d-]", ",");
+			
+			sNegatives = sNegatives.replaceAll(",{2,}", ","); //replaces double+ commas with one comma
+			
+			if (sNegatives.charAt(0) == ',' && sNegatives.length() != 1) {
+				while (sNegatives.charAt(0) == ',') {
+					sNegatives = sNegatives.replaceFirst(",", "");
+				}
+				while (sNegatives.charAt(sNegatives.length() - 1) == ',') {
+					sNegatives = sNegatives.replace(sNegatives.substring(sNegatives.length() - 1), "");
+				}
+			}
+			String[] t = sNegatives.split(",");
+			for (int a = 0; a < t.length; a++) {
+				negatives.add(Integer.parseInt(t[a]));
+				if (negatives.get(a) != Math.abs(negatives.get(a))) { //What to do with negatives
+				}
+			}
+			for (int num: negatives) {
+				if (num != Math.abs(num)) {
+					throw new IndexOutOfBoundsException();
+				}
+			}
+		} catch (IndexOutOfBoundsException ie) {
+			String error = "Negatives are not allowed: ";
+			for (int u = 0; u < negatives.size(); u++) {
+				if (u != negatives.size() - 1) {
+					error += negatives.get(u) + ", ";
+				} else {
+					error += negatives.get(u);
+				}
+			}
+			System.out.println(error);
+			return negatives.get(0); //What to do after exception is thrown like instructions say
+			
+		} catch (Exception e) {
+			//just keep going if this catches
+		}
+		
+		
+		s = s.replaceAll("\\D", ","); //replace non digit elements to make this fit the right format to work
+		
+		s = s.replaceAll(",{2,}", ","); //replaces double+ commas with one comma
+		
+		if (s.charAt(0) == ',' && s.length() != 1) {
+			while (s.charAt(0) == ',') {
+				s = s.replaceFirst(",", "");
+			}
+			while (s.charAt(s.length() - 1) == ',') {
+				s = s.replace(s.substring(s.length() - 1), "");
+			}
+		}
+		
+		String[] sEdit = s.split(",");
+		
+		//All delimiters should be taken care of, I will test it out
+		
+		//Check for numbers bigger that 1000
+		for (int l = 0; l < sEdit.length; l++) {
+			try {
+				int j = Integer.parseInt(sEdit[l]);
+				if (j > 1000) {
+					sEdit[l] = "0";
+				}
+				
+			} catch (Exception e) {
+				//just keep going
+			}
+		}
+
+		try {
+			if (s.equals(",")) { //if it's blank or they add a non numeric
+				return 0;
+			} else if (sEdit.length == 1) { //if there's 1 number
+				return Integer.parseInt(sEdit[0]);
+			} else { //for more numbers
+				int total = 0;
+				for (int n = 0; n < sEdit.length; n++) {
+					total += Integer.parseInt(sEdit[n]);
+				}
+				return total;
+			}
+		} catch (Exception e) {
+			System.out.println("Error. Something went wrong.");
+			return -1;
+		}
+		
+	}
+	
+	
+
+}
---
+++ src/StringCalcTests.java
@@ -0,0 +1,135 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class StringCalcTests {
+
+	@Test
+	void test1() {
+		int expected = 0;
+		int actual = StringCalc.add("");
+		Assert.assertEquals("Failure -- Didn't match expected value", expected, actual);
+	}
+	
+	@Test
+	void test2() {
+		int expected = 1;
+		int actual = StringCalc.add("1");
+		Assert.assertEquals("Failure -- Didn't match expected value", expected, actual);
+	}
+	
+	@Test
+	void test3() {
+		int expected = 3;
+		int actual = StringCalc.add("1,2");
+		Assert.assertEquals("Failure -- Didn't match expected value", expected, actual);
+	}
+	
+	@Test
+	void test4() {
+		int expected = 10;
+		int actual = StringCalc.add("1,2,3,4");
+		Assert.assertEquals("Failure -- Didn't match expected value", expected, actual);
+	}
+	
+	@Test
+	void test5() {
+		int expected = 6;
+		int actual = StringCalc.add("1\n2,3");
+		Assert.assertEquals("Failure -- Didn't match expected value", expected, actual);
+	}
+	
+	//I'm adding extra tests as I go, just as I think of them
+	@Test
+	void test6() {
+		int expected = 0;
+		int actual = StringCalc.add("&");
+		Assert.assertEquals("Failure -- Didn't match expected value", expected, actual);
+	}
+	
+	@Test
+	void test7() {
+		int expected = 6;
+		int actual = StringCalc.add("1\n$2,3");
+		Assert.assertEquals("Failure -- Didn't match expected value", expected, actual);
+	}
+	
+	@Test
+	void test8() { //Extra test - see that it replaces blank spaces with commas
+		int expected = 6;
+		int actual = StringCalc.add("1 2,3");
+		Assert.assertEquals("Failure -- Didn't match expected value", expected, actual);
+	}
+	
+	@Test
+	void test9() { //Extra test - first make sure it simply gets rid of 
+		int expected = 3; //beginning character that are not digits
+		int actual = StringCalc.add("//:1,2");
+		Assert.assertEquals("Failure -- Didn't match expected value", expected, actual);
+	}
+	
+	@Test
+	void test10() {
+		int expected = 3;
+		int actual = StringCalc.add("//;\n1;2");
+		Assert.assertEquals("Failure -- Didn't match expected value", expected, actual);
+	}
+	
+	@Test //extra test
+	void test11() {
+		int expected = 0;
+		int actual = StringCalc.add("&^");
+		Assert.assertEquals("Failure -- Didn't match expected value", expected, actual);
+	}
+	
+	@Test //extra test for more delimiters
+	void test12() {
+		int expected = 14;
+		int actual = StringCalc.add("3@5#6");
+		Assert.assertEquals("Failure -- Didn't match expected value", expected, actual);
+	}
+	
+	@Test //test for characters at end of string
+	void test13() {
+		int expected = 6;
+		int actual = StringCalc.add("3,2,1%$");
+		Assert.assertEquals("Failure -- Didn't match expected value", expected, actual);
+	}
+	
+	@Test //test for negative numbers
+	void test14() {
+		int expected = -4;
+		int actual = StringCalc.add(",-4#");
+		Assert.assertEquals("Failure -- Didn't match expected value", expected, actual);
+	}
+	
+	@Test //test for multiple negative numbers
+	void test15() {
+		int expected = -4;
+		int actual = StringCalc.add(",-4#,-10, -5");
+		Assert.assertEquals("Failure -- Didn't match expected value", expected, actual);
+	}
+	
+	@Test
+	void test16() {
+		int expected = 2;
+		int actual = StringCalc.add("2,1001");
+		Assert.assertEquals("Failure -- Didn't match expected value", expected, actual);
+	}
+	
+	@Test
+	void test17() {
+		int expected = 6;
+		int actual = StringCalc.add("//[***]\\n1***2***3");
+		Assert.assertEquals("Failure -- Didn't match expected value", expected, actual);
+	}
+	
+	@Test
+	void test18() {
+		int expected = 6;
+		int actual = StringCalc.add("//[*][%]\n1*2%3");
+		Assert.assertEquals("Failure -- Didn't match expected value", expected, actual);
+	}
+	
+}
